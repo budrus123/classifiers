a456e0721283d00149cfb83a650718c95dd23719
Mahmoud Abdelkareem <budrus123@gmail.com>
2021-02-14 19:10:03 -0600

    started on perceptron
    - started work on perceptron
    - made a file for iris which is linearly separable

--- classifiers/preceptron.py
+++ classifiers/preceptron.py
@@ -7,17 +7,48 @@ from sklearn.model_selection import train_test_split
 
 class Preceptron:
     def __init__(self):
-        print()
+        self.weights = np.array([])
+        self.learning_rate = 0.1
 
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
+        number_of_weights = X_train.shape[1]
+        number_of_instances = X_train.shape[0]
+        print(X_train.shape)
+        self.weights = np.random.rand(number_of_weights)  * 2 + 1
+        print(self.weights)
+        counter = 0
+        wrong_counter = 11
+        while wrong_counter > 0:
+            wrong_counter = 0
+            for i in range(number_of_instances):
+                # TODO REMOVE THIS, To handle missing data, TODO REMOVE THIS
+                if len(X_train[i].data) != len(self.weights):
+                    continue
+                y_prime = self.predict([X_train[i].data])
+                difference = (y_train[i] - y_prime)
+                if difference != 0:
+                    wrong_counter += 1
+                else:
+                    continue
+                delta_weights = self.learning_rate * difference * X_train[i].data
+                # print(delta_weights)
+                self.weights += delta_weights
+                # print(difference)
+            print('wrong counter: '+str(wrong_counter))
+        print(self.weights)
 
     def predict(self, instances):
         predictions = np.array([])
+        sum = 0
+
         for instance in instances:
-            print()
-        return predictions
+            summation = np.dot(instance.T.data,self.weights)
+        # print(summation)
+        if summation > 0:
+            return 2
+        return 1
 
     def find_k_nearest_instances(self, new_instance):
         print()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.datasets import load_svmlight_file
 from sklearn.model_selection import train_test_split
 from classifiers.knn import KNN
+from classifiers.preceptron import Preceptron
 
 dataset_subsample = 150
 
@@ -10,7 +11,7 @@ def load_dataset(file_name):
     return data[0], data[1]
 
 def main():
-    X, y = load_dataset("datasets/multiclass/iris.scale")
+    X, y = load_dataset("datasets/multiclass/iris_seprable.scale")
     y = y[0:dataset_subsample]
     X = X[0:dataset_subsample, ]
     X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3)
@@ -18,12 +19,16 @@ def main():
     diabetes_instance = np.array([3.000000, 162.000000, 52.000000, 38.000000, 0.000000, 37.200001, 0.652000, 24.000000])
     iris_instance = np.array([0.388889, -0.166667, 0.525424, 0.666667])
 
-    instances = np.array([iris_instance,iris_instance])
-    knn = KNN()
-    knn.fit(X_train, y_train)
-    predictions = knn.predict(instances)
+    # instances = np.array([iris_instance,iris_instance])
+    # knn = KNN()
+    # knn.fit(X_train, y_train)
+    #
+    perc = Preceptron()
+    perc.fit(X_train, y_train)
+
+    # predictions = knn.predict(instances)
     # print(predictions)
-    print(knn.score(X_test, y_test))
+    # print(knn.score(X_test, y_test))
 
 if __name__ == '__main__':
     main()
