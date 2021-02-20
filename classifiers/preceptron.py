96205ba27c23bd93ad382a3272d4f65d5fd74b39
Mahmoud Abdelkareem <budrus123@gmail.com>
2021-02-19 20:45:57 -0600

    additions to perceptron
    additions to perceptron

--- classifiers/preceptron.py
+++ classifiers/preceptron.py
@@ -44,5 +44,27 @@ class Preceptron:
             return 2
         return 1
 
-    def score(self, X_test, y_test):
-        return 0
+
+
+    # TODO: Compare the below with the old above code
+    def train_perceptron(self, X_train, y_train):
+        number_of_weights = X_train.shape[1]
+        number_of_instances = X_train.shape[0]
+        self.weights = np.random.rand(number_of_weights)  * 2 + 1
+        self.weights = np.array([0] * number_of_instances)
+        wrong_counter = 0
+        while wrong_counter > 0:
+            wrong_counter = 0
+            for i in range(number_of_instances):
+                y_predict = self.predict([X_train[i]])
+                if y_train[i] != y_predict:
+                    wrong_counter += 1
+                    delta_weights = self.learning_rate * y_train[i] * X_train[i].A[0].data
+                    self.weights += delta_weights
+
+    def predict(self, instance):
+        instance_data_array = instance.A[0]
+        prediction = np.dot(instance_data_array.T.data,self.weights)
+        if prediction > 0:
+            return 1
+        return -1
