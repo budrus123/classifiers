43a0a4a333c38196c92d389fa2051bf859e03b71
Mahmoud Abdelkareem <budrus123@gmail.com>
2021-03-03 18:25:27 -0600

    Added ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# K-NN and Perceptron Classifers
+# K-NN and Perceptron Classifiers
+
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * K-NN Classifier
+ * Perceptron Classifier
+
+INTRODUCTION
+------------
+In this project, two classifiers were created. The first classifier was a K-NN classifier and the second one was a simple Perceptron.
+Both were created from scratch and both used two different datasets.
+The first dataset was the a4a dataset, which is a binary outcome dataset that can have an outcome of either +1 or -1.
+The other was the famous multiclass Iris dataset which can have an outcome of 1, 2, or 3.
+
+
+Usage
+------------
+
+```sh
+$ python main.py <image_name.jpg>
+```
+
--- main.py
+++ main.py
@@ -8,12 +8,11 @@ from classifiers.Preceptron import Preceptron
 from classifiers.MulticlassPerceptron import MulticlassPreceptron
 import matplotlib.pyplot as plt
 
-
-
 '''
 Function to load the data set and return the feature
 vector and the output vector.
 '''
+
 def load_data_set(file_name):
     data = load_svmlight_file(file_name)
     return data[0], data[1]
@@ -23,6 +22,7 @@ def load_data_set(file_name):
 Function that splits the data into training and testing
 data and sub-samples the data to a certain size (randomly).
 '''
+
 def split_data_set(X, y, test_size, subsample_size):
     if subsample_size > X.shape[0]:
         subsample_size = X.shape[0]
@@ -42,6 +42,7 @@ def split_data_set(X, y, test_size, subsample_size):
 '''
 Function to visualize the weights.
 '''
+
 def visualize_weights(weights, number_of_features):
     if isinstance(weights, dict):
         visualize_weight_object(weights, number_of_features)
@@ -53,6 +54,7 @@ def visualize_weights(weights, number_of_features):
 Function to visualize the weights when the weights are 
 a dictionary (weights for different classes).
 '''
+
 def visualize_weight_object(weights, number_of_features):
     fig, ax = plt.subplots(len(weights), sharex=True)
     features = np.arange(number_of_features)
@@ -67,10 +69,12 @@ def visualize_weight_object(weights, number_of_features):
     fig.tight_layout(pad=1.0)
     plt.show()
 
+
 '''
 Function to visualize the weights when the weights are 
 an array (weights for binary +1/-1 data set).
 '''
+
 def visualize_weight_array(weights, number_of_features):
     fig, ax = plt.subplots()
     features = np.arange(number_of_features)
@@ -81,10 +85,12 @@ def visualize_weight_array(weights, number_of_features):
     fig.tight_layout(pad=1.0)
     plt.show()
 
+
 '''
 Function to plot the different values of the accuracy
 along with different values of K.
 '''
+
 def plot_accuracy_for_k_values(k_values, accuracy):
     fig, ax = plt.subplots()
     ax.plot(k_values, accuracy)
@@ -106,9 +112,11 @@ def main():
     knn.fit(X_train, y_train)
     predictions = knn.predict(X_test)
     score = knn.score(X_test, y_test)
-    print(score)
+    print("----------- K-NN Classifier ----------- ")
+    print("Score of K-nn is: " + str(score))
 
     # Perceptron Code here
+    print("----------- Perceptron ----------- ")
     possible_classes = [int(element) for element in set(y_train)]
     if len(possible_classes) == 2:
         perceptron = Preceptron()
@@ -117,7 +125,8 @@ def main():
 
     weights = perceptron.train_perceptron(X_train, y_train)
     score = perceptron.score(X_test, y_test)
-    print(score)
+    print("Score of perceptron is: " + str(score))
+    print("Visualizing weight importance")
     visualize_weights(weights, X_train.shape[1])
 
 
